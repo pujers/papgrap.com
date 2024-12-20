3ea01fe300d16430bb52a4865b051034c4bc8aee
Puja Sari <puja.resma@gmail.com>
2024-12-20 02:50:05 +0100

    Added Models.py and added jokes.

--- grappen/models.py
+++ grappen/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Grap(models.Model):   #grap is the model name means joke in Dutch
+    question = models.TextField(max_length=200)
+    answer = models.TextField(max_length=100, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+    
+
+    #from grappen.models import Grap ----use it in shell to import the model
