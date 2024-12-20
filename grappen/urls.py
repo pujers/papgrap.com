86b7ddd72fe56f4e3ae5e87540256611335a149a
Puja Sari <puja.resma@gmail.com>
2024-12-20 15:03:05 +0100

    Edited grap_form to update/new jokes submitted

--- grappen/urls.py
+++ grappen/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import (GrapCreateView, GrapDetailView, GrapListView, 
-                    GrapUpdateView)
+from .views import (
+    GrapCreateView, GrapDetailView, GrapListView, GrapUpdateView
+)
 
 app_name = 'grappen'
 
